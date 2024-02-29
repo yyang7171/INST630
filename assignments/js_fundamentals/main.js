9c900f025f6a4baeee6d17ae41cc620fee904963
Yabing Yang <98364345+yyang7171@users.noreply.github.com>
2024-02-29 15:33:04 -0500

    updates link and js

---
+++ assignments/js_fundamentals/main.js
@@ -0,0 +1,44 @@
+const userName = "yyang";
+
+userName === "yyang"
+  ? console.log(`Hello, ${userName}!`)
+  : console.log("Hello!");
+
+const userQuestion = "What is the answer to my question?";
+
+console.log(`yyang: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+
+switch (randomNumber) {
+  case 0:
+    eightBall = "It is certain";
+    break;
+  case 1:
+    eightBall = "It is decidedly so";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    eightBall = "Do not count on it";
+    break;
+  case 5:
+    eightBall = "My sources say no";
+    break;
+  case 6:
+    eightBall = "Outlook not so good";
+    break;
+  case 7:
+    eightBall = "Signs point to yes";
+    break;
+  default:
+    console.log("Sorry, please try again.");
+}
+
+console.log(eightBall);
